f119bd14738dedb980a2022e7dc308af74f67ec3
Raphael <oizueke@conestogac.on.ca>
2025-11-26 22:31:12 -0500

    code clean up

--- src/BinarySearch/SqrtxSolution.java
+++ src/BinarySearch/SqrtxSolution.java
@@ -8,20 +8,16 @@ public class SqrtxSolution {
         //edge case
         if(sqrtNo ==0) return 0;
         //form array in ascending form up to the desired square root number
-        int[] searchArray = new int[sqrtNo];
-        for(int i=0; i<sqrtNo; i++){
-            searchArray[i] = i+1;
-        }
+        int[] arrayToSrtNo = formArrayToN(sqrtNo);
 
         //search square root with binary search
-        return binarySearchSqrt(searchArray,sqrtNo);
+        return binarySearchSqrt(arrayToSrtNo,sqrtNo);
     }
 
     public static  int binarySearchSqrt(int[] searchArray, int sqrtNo){
         //binary search to find number in array when squared gives target
         int start = 0;
         int end = searchArray.length - 1;
-
 
         while (start<=end){
             int middle = start + ((end -start)/2);
@@ -37,9 +33,18 @@ public class SqrtxSolution {
             }
         }
 
-        //end holds the closes value to the squaree root after iteration terminates
+        //end holds the closes value to the square root after iteration terminates
         return searchArray[end];
     }
+
+    public static int[] formArrayToN(int arrayLength){
+        int[] formedArray = new int[arrayLength];
+        for(int i=0; i<arrayLength; i++){
+            formedArray[i] = i+1;
+        }
+        return formedArray;
+    }
+
 }
 
 
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import src.BinarySearch.SqrtxSolution;
 
 public class Main {
     public static void main(String[] args) {
+        //test Binary search to find square root
         System.out.println(SqrtxSolution.mySqrt(8));
     }
 }
