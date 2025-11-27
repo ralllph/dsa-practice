8c563085eb5918ea097192ffb9d46aaf8fc2fe0c
Raphael <oizueke@conestogac.on.ca>
2025-11-26 22:20:47 -0500

    handled edge cases

--- src/BinarySearch/SqrtxSolution.java
+++ src/BinarySearch/SqrtxSolution.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 public class SqrtxSolution {
 
     public static int mySqrt(int sqrtNo) {
+        //edge case
+        if(sqrtNo ==0) return 0;
         //form array in ascending form up to the desired square root number
         int[] searchArray = new int[sqrtNo];
         for(int i=0; i<sqrtNo; i++){
@@ -12,7 +14,7 @@ public class SqrtxSolution {
         }
 
         //search square root with binary search
-        return binarySearchSqrt(searchArray,8);
+        return binarySearchSqrt(searchArray,sqrtNo);
     }
 
     public static  int binarySearchSqrt(int[] searchArray, int sqrtNo){
@@ -20,12 +22,15 @@ public class SqrtxSolution {
         int start = 0;
         int end = searchArray.length - 1;
 
+
         while (start<=end){
             int middle = start + ((end -start)/2);
+            //handling edge case integer overflow
+            long squaredValue = ((long) searchArray[middle] * searchArray[middle]);
 
-            if((searchArray[middle] * searchArray[middle]) > sqrtNo){
+            if(squaredValue> sqrtNo){
                 end  = middle - 1;
-            } else if ((searchArray[middle] * searchArray[middle]) < sqrtNo) {
+            } else if (squaredValue < sqrtNo) {
                 start = middle + 1;
             }else {
                 return searchArray[middle];
