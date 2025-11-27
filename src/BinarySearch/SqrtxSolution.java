45f99c0ab27c573627ff06d79bd947e9dbecfdb9
Raphael <oizueke@conestogac.on.ca>
2025-11-26 20:49:58 -0500

    first thought: form array in asc up to square root number

--- src/BinarySearch/SqrtxSolution.java
+++ src/BinarySearch/SqrtxSolution.java
@@ -1,4 +1,21 @@
 package src.BinarySearch;
 
+import java.util.Arrays;
+
 public class SqrtxSolution {
+
+    public static int mySqrt(int sqrtNo) {
+        //form array in ascending form up to the desired square root number
+        int[] searchArray = new int[sqrtNo];
+        for(int i=0; i<sqrtNo; i++){
+            searchArray[i] = i+1;
+        }
+
+        return 1;
+    }
+
+
+
 }
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 package src;
 
+import src.BinarySearch.SqrtxSolution;
+
 public class Main {
     public static void main(String[] args) {
-
+        SqrtxSolution.mySqrt(16);
     }
 }
