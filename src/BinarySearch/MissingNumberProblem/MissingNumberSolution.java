f068cd89fb2023c77bad187babd462c0222b472d
Raphael <oizueke@conestogac.on.ca>
2025-11-27 19:02:53 -0500

    pacjkage restructure and new problem added

---
+++ src/BinarySearch/MissingNumberProblem/MissingNumberSolution.java
@@ -0,0 +1,4 @@
+package src.BinarySearch.MissingNumberProblem;
+
+public class MissingNumberSolution {
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 package src;
 
-import src.BinarySearch.SqrtxSolution;
+import src.BinarySearch.SqrtProblem.SqrtxSolution;
 
 public class Main {
     public static void main(String[] args) {
         //test Binary search to find square root
         System.out.println(SqrtxSolution.mySqrt(8));
     }
+
 }
