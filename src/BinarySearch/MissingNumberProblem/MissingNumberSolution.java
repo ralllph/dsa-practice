0c866b5905ade6d0c8dac40b5eee724a6250fcdb
Raphael <oizueke@conestogac.on.ca>
2025-11-28 15:55:58 -0500

    brute force first attempt with test in main

--- src/BinarySearch/MissingNumberProblem/MissingNumberSolution.java
+++ src/BinarySearch/MissingNumberProblem/MissingNumberSolution.java
@@ -1,4 +1,40 @@
 package src.BinarySearch.MissingNumberProblem;
 
 public class MissingNumberSolution {
+
+    public int missingNumber(int[] nums){
+        return searchMissingNumber(nums);
+    }
+
+    public static int searchMissingNumber(int[] arrayWithNumbers){
+        int arrayLength = arrayWithNumbers.length;
+        int start  = 0;
+        int end = arrayLength - 1;
+        int numberFound = 0;
+
+        for(int i=0; i<arrayLength; i++){
+
+                int target = arrayWithNumbers[i];
+
+                while (start <= end) {
+                    int middle = start + ((end - start) / 2);
+                    if (middle > target) {
+                        end = middle - 1;
+                    } else if (middle < target) {
+                        start = middle + 1;
+                    }else{
+                        break;
+                    }
+
+                }
+
+
+             numberFound = i;
+
+        }
+
+        return numberFound;
+
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 package src;
 
+import src.BinarySearch.MissingNumberProblem.MissingNumberSolution;
 import src.BinarySearch.SqrtProblem.SqrtxSolution;
 
 public class Main {
     public static void main(String[] args) {
         //test Binary search to find square root
         System.out.println(SqrtxSolution.mySqrt(8));
+        System.out.println(MissingNumberSolution.searchMissingNumber((new int[] {9,6,4,2,3,5,7,0,1})));
     }
 
 }
