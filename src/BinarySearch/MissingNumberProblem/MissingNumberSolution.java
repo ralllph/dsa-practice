4ff3569833bafd720cdd6cae5743d2013d11268c
Raphael <oizueke@conestogac.on.ca>
2025-11-30 18:51:23 -0500

    search by boundary solution completed

--- src/BinarySearch/MissingNumberProblem/MissingNumberSolution.java
+++ src/BinarySearch/MissingNumberProblem/MissingNumberSolution.java
@@ -12,8 +12,21 @@ public class MissingNumberSolution {
         int end = arrayWithNumbers.length;
 
         while(start <end){
-
+            int middle = start + ((end - start)/2);
+            if(arrayWithNumbers[middle] > middle){
+                //check left for previous occurrence, check from middle value to left for
+                // possible previous occurrence
+                end = middle;
+            }else{
+                //the number isn't greater than index  meaning first occurrence of +1 change
+                // is to the right
+                start = middle + 1;
+            }
         }
+
+        //s At this point start == end. so start index and end index points to the same number
+        // that was first incremented by 1
+        return start;
     }
 
 }
