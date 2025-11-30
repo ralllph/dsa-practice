a4ac093833cd64f9f5c76047813d1d593b821b42
Raphael <oizueke@conestogac.on.ca>
2025-11-30 18:18:29 -0500

    brute attempt failed test case, using binary search by boundary attempt

--- src/BinarySearch/MissingNumberProblem/MissingNumberSolution.java
+++ src/BinarySearch/MissingNumberProblem/MissingNumberSolution.java
@@ -1,51 +1,14 @@
 package src.BinarySearch.MissingNumberProblem;
 
+import java.util.Arrays;
+
 public class MissingNumberSolution {
 
     public static int searchMissingNumber(int[] arrayWithNumbers){
-        //changing attempt , sorting array first
-        int arrayLength = arrayWithNumbers.length;
-        int numberFound = 0;
-        boolean isFound = false;
-        boolean inArrayWithNums = false;
-
-
-        for(int i=0; i<=arrayLength; i++){
-            int start  = 0;
-            int end = arrayLength-1 ;
-
-
-            if(i < arrayLength){
-                //int target = arrayWithNumbers[i];
-
-                while (start <= end) {
-                    int middle = start + ((end - start) / 2);
-                    if (i > arrayWithNumbers[middle]) {
-                        start = middle + 1;
-                    } else if (i < arrayWithNumbers[middle]) {
-                        end = middle - 1;
-                    }else{
-                        inArrayWithNums = true;
-                        break;
-                    }
-                    inArrayWithNums = false;
-                }
-
-
-                if(!inArrayWithNums) return i;
-
-            }else{
-                isFound = true;
-            }
-
-
-
-                if(isFound) numberFound = i;
-
-        }
-
-        return numberFound;
-
+        //using binary search by boundary
+        //sort array
+        Arrays.sort(arrayWithNumbers);
+        int start = 0;
     }
 
 }
