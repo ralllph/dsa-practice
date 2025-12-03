0fedd9b27e4c735d4350c383518f35c52981ae3f
Raphael <oizueke@conestogac.on.ca>
2025-12-03 18:04:21 -0500

    using the binary search by boundary approac

--- src/BinarySearch/FirstBadVersion/FirstBadVersionSolution.java
+++ src/BinarySearch/FirstBadVersion/FirstBadVersionSolution.java
@@ -1,5 +1,10 @@
 package src.BinarySearch.FirstBadVersion;
 
 public class FirstBadVersionSolution {
+    public int firstBadVersion(int n) {
+        int start  = 0;
+        int end = n;
 
+        while(start)
+    }
 }
