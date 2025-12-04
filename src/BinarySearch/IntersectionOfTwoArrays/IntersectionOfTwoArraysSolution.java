91f3dc7a34bf6ed7803f88b1d03bfc3c792d9bd2
Raphael <oizueke@conestogac.on.ca>
2025-12-03 21:31:27 -0500

    added function to convert arry list to array without duplicates

--- src/BinarySearch/IntersectionOfTwoArrays/IntersectionOfTwoArraysSolution.java
+++ src/BinarySearch/IntersectionOfTwoArrays/IntersectionOfTwoArraysSolution.java
@@ -8,18 +8,20 @@ public class IntersectionOfTwoArraysSolution {
         //binary search by value approach
         if(nums1.length > nums2.length){
             Arrays.sort(nums1);
+            return arrayIntersection(nums1,nums2);
         }else{
             Arrays.sort(nums2);
+            return arrayIntersection(nums2,nums1);
         }
 
     }
 
     public int[] arrayIntersection(int[] larger, int[] smaller){
         ArrayList<Integer> arrayIntersection = new ArrayList<Integer>();
-        int start = 0;
-        int end = larger.length - 1;
-        for(int i = 0;  i<smaller.length; i++){
 
+        for(int i = 0;  i<smaller.length; i++){
+            int start = 0;
+            int end = larger.length - 1;
             while(start<=end){
                 int middle = start + ((end -start)/2);
 
@@ -29,13 +31,21 @@ public class IntersectionOfTwoArraysSolution {
                     end = middle - 1;
                 }else{
                     arrayIntersection.add(smaller[i]);
+                    break;
                 }
 
             }
 
         }
 
-        return arrayIntersection.stream().distinct().toArray()
+        return copyArrayListToArray(arrayIntersection);
+    }
+
+    public int[] copyArrayListToArray(ArrayList<Integer> arrayToConvert){
+        return arrayToConvert.stream()
+                .distinct()
+                .mapToInt(Integer::intValue)
+                .toArray();
     }
 
 }
