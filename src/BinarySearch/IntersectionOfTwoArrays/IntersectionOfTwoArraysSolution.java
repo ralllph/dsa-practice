ce674687858d457bda5fb425d5225af755945ee2
Raphael <oizueke@conestogac.on.ca>
2025-12-03 19:30:46 -0500

    sorting the larger array

--- src/BinarySearch/IntersectionOfTwoArrays/IntersectionOfTwoArraysSolution.java
+++ src/BinarySearch/IntersectionOfTwoArrays/IntersectionOfTwoArraysSolution.java
@@ -1,8 +1,15 @@
 package src.BinarySearch.IntersectionOfTwoArrays;
 
+import java.util.Arrays;
+
 public class IntersectionOfTwoArraysSolution {
     public int[] intersection(int[] nums1, int[] nums2) {
         //binary search by value approach
+        if(nums1.length > nums2.length){
+            Arrays.sort(nums1);
+        }else{
+            Arrays.sort(nums2);
+        }
 
     }
 }
