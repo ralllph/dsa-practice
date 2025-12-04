11b0b56a1bb06d2e9bfc237e58f42e98365d3179
Raphael <oizueke@conestogac.on.ca>
2025-12-03 19:28:17 -0500

    taking binary search by value approach

--- src/BinarySearch/IntersectionOfTwoArrays/IntersectionOfTwoArraysSolution.java
+++ src/BinarySearch/IntersectionOfTwoArrays/IntersectionOfTwoArraysSolution.java
@@ -1,5 +1,8 @@
 package src.BinarySearch.IntersectionOfTwoArrays;
 
 public class IntersectionOfTwoArraysSolution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        //binary search by value approach
 
+    }
 }
