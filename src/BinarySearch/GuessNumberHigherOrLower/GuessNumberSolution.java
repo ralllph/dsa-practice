681d95b6cee29899e73b63cb8a92383ad7598aad
Raphael <oizueke@conestogac.on.ca>
2025-12-11 17:38:06 -0500

    binary search using guess api on leetcode completed

--- src/BinarySearch/GuessNumberHigherOrLower/GuessNumberSolution.java
+++ src/BinarySearch/GuessNumberHigherOrLower/GuessNumberSolution.java
@@ -10,6 +10,25 @@ public class GuessNumberSolution {
             return - 1;
         }
 
+        while(start<=end){
+            int middle = start + ((end- start)/2);
+            //no access to guess  function from leet code
+            int guessed = guess(middle);
+
+            if(guessed == 0){
+                return middle;
+            }
+            else if(guessed == -1){
+                end =middle - 1;
+            }
+            else if(guessed == 1){
+                start = middle + 1;
+            }
+
+        }
+
+        return -1;
+
     }
 
 }
