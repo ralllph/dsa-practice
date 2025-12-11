f59e2fd24f4decea566ab6c8b582aea3d5c81ddd
Raphael <oizueke@conestogac.on.ca>
2025-12-11 16:53:35 -0500

    applying binary search by value approach to half the number

--- src/BinarySearch/ValidPerfectSquare/ValidPerfectSquareSolution.java
+++ src/BinarySearch/ValidPerfectSquare/ValidPerfectSquareSolution.java
@@ -1,4 +1,12 @@
 package src.BinarySearch.ValidPerfectSquare;
 
 public class ValidPerfectSquareSolution {
+    public boolean isPerfectSquare(int num) {
+        int start = 1;
+        int end = num/2;
+
+        while(start<= end){
+
+        }
+    }
 }
