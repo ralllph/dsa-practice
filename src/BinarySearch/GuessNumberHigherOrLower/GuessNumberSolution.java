52369859fe8aaa144942777adf4f3eb6bedbda2f
Raphael <oizueke@conestogac.on.ca>
2025-12-11 17:29:32 -0500

    applying bunary search by value from 1 to passed number

--- src/BinarySearch/GuessNumberHigherOrLower/GuessNumberSolution.java
+++ src/BinarySearch/GuessNumberHigherOrLower/GuessNumberSolution.java
@@ -1,5 +1,10 @@
 package src.BinarySearch.GuessNumberHigherOrLower;
 
 public class GuessNumberSolution {
+    //This problem extends a Guess Game class in leet code
+    public int guessNumber(int n){
+        int start =  1;
+        int end  =n;
+    }
 
 }
