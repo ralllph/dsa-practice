91c8652e9498caa3f1b00c263e6747a58edfe116
Raphael <oizueke@conestogac.on.ca>
2025-12-11 17:07:06 -0500

    initial search by value attempt completed

--- src/BinarySearch/ValidPerfectSquare/ValidPerfectSquareSolution.java
+++ src/BinarySearch/ValidPerfectSquare/ValidPerfectSquareSolution.java
@@ -2,11 +2,23 @@ package src.BinarySearch.ValidPerfectSquare;
 
 public class ValidPerfectSquareSolution {
     public boolean isPerfectSquare(int num) {
+        if(num ==1) return true;
         int start = 1;
         int end = num/2;
 
         while(start<= end){
+            int middle = start + ((end-start)/2);
+            if(middle * middle > num){
+                end = middle -1;
+            }else if(middle * middle < num){
+                start = middle + 1;
+            }else{
+                return true;
+            }
 
         }
+
+        return false;
     }
+
 }
